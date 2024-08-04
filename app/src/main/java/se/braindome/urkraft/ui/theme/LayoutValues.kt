213fcb5bf608ba625afc7386b154ffdd6be6bd29
Antonio Saracino <antonio.saracino83@gmail.com>
2024-08-04 15:12:56 +0200

    Added some layut values

---
+++ app/src/main/java/se/braindome/urkraft/ui/theme/LayoutValues.kt
@@ -0,0 +1,20 @@
+package se.braindome.urkraft.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object LayoutValues {
+
+    val smallPadding = 8.dp
+    val mediumPadding = 16.dp
+    val largePadding = 24.dp
+
+    val smallMargin = 8.dp
+    val mediumMargin = 16.dp
+    val largeMargin = 24.dp
+
+    val smallCornerRadius = RoundedCornerShape(4.dp)
+    val mediumCornerRadius = RoundedCornerShape(8.dp)
+    val largeCornerRadius = RoundedCornerShape(16.dp)
+
+}
