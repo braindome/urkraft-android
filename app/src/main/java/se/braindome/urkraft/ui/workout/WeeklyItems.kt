02850a0c24f840b7b5b04a31ef0bee83c6ac209f
Antonio Saracino <antonio.saracino83@gmail.com>
2024-08-30 16:17:10 +0200

    Weekly card and planning view WIP

--- app/src/main/java/se/braindome/urkraft/ui/workout/WeeklyItems.kt
+++ app/src/main/java/se/braindome/urkraft/ui/workout/WeeklyItems.kt
@@ -1,41 +1,104 @@
 package se.braindome.urkraft.ui.workout
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WeeklyCard() {
     val weekDays = listOf("M", "T", "W", "T", "F", "S", "S")
     ElevatedCard(
-        modifier = Modifier.fillMaxWidth().height(800.dp)
+        colors = CardColors(
+            containerColor = Color.LightGray,
+            contentColor = Color.Black,
+            disabledContentColor = Color.White,
+            disabledContainerColor = Color.White
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-        ) {
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+
+            ) {
             weekDays.forEach { day ->
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start
+                Column(
+                    modifier = Modifier
+                        .height(64.dp)
                 ) {
-                    Text(
-                        text = day,
-                        modifier = Modifier.padding(start = 16.dp)
+                    Row(
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Text(
+                            text = day,
+                            modifier = Modifier.padding(start = 8.dp).size(24.dp)
+                        )
+                        WeeklyCardItem()
+                    }
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .height(1.dp)
+                            .padding(
+                                top = 16.dp,
+                                start = 32.dp,
+                                end = 16.dp
+                            )
                     )
                 }
+
             }
         }
+    }
+}
+
+@Composable
+fun WeeklyCardItem() {
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            //.background(Color.Yellow)
+            .height(56.dp)
+            .fillMaxWidth(),
+
+    ) {
+        Text(
+            lineHeight = 32.sp,
+            text = "This is a workout",
+            modifier = Modifier
+                .shadow(1.dp, RoundedCornerShape(15.dp))
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(32.dp)
+                .padding(start = 8.dp)
+
+        )
     }
 }
 
--- app/src/main/java/se/braindome/urkraft/ui/workout/WeeklyPlanningScreen.kt
+++ app/src/main/java/se/braindome/urkraft/ui/workout/WeeklyPlanningScreen.kt
@@ -1,24 +1,43 @@
 package se.braindome.urkraft.ui.workout
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WeeklyPlanningScreen() {
-    Column(
+    val numberOfWeeks = 4
+
+    Box(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Weekly Planning",
-            style = MaterialTheme.typography.labelLarge
-        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Weekly Planning", style = MaterialTheme.typography.labelLarge)
+            LazyColumn {
+                items(numberOfWeeks) {
+                    WeeklyCard()
+                }
+            }
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeeklyPlanningScreenPreview() {
+    WeeklyPlanningScreen()
 }
