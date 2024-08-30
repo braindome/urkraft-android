4ce794e5976046c66d9ae94f4c8292f0c6ce3643
Antonio Saracino <antonio.saracino83@gmail.com>
2024-08-30 12:38:55 +0200

    Weekly View WIP

--- app/src/main/java/se/braindome/urkraft/ui/workout/WeeklyItems.kt
+++ app/src/main/java/se/braindome/urkraft/ui/workout/WeeklyItems.kt
@@ -1,4 +1,46 @@
 package se.braindome.urkraft.ui.workout
 
-class WeeklyItems {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WeeklyCard() {
+    val weekDays = listOf("M", "T", "W", "T", "F", "S", "S")
+    ElevatedCard(
+        modifier = Modifier.fillMaxWidth().height(800.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            weekDays.forEach { day ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = day,
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeeklyCardPreview() {
+    WeeklyCard()
 }
